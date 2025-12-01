96223c6a62b682ef250eafa4c61283de77fe668c
Agung Sedayu <agungsedayuuu28@gmail.com>
2025-12-01 11:01:41 +0700

    Update tittle and add headers streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("Solusi Integral Kaidah dengan Pias Titik Tengah")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Solusi Integral Tentu Numerik")
+st.headers("Kaidha Pias Titik Tengah")
